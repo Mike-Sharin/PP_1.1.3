6e5bacc4b3171f538bcdf1bb077228fed15d1f3d
Mike-Sharin <mike.sharin@gmail.com>
2022-08-30 01:25:47 +0300

    Дополнительно: После детельного изучения темы, заметил ошибки, поэтому подправил код с новыми вводными.

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    //@SuppressWarnings("FieldCanBeLocal")
     private final String SQL_CREATE_TABLE = "CREATE TABLE IF NOT EXISTS users ( id bigint NOT NULL AUTO_INCREMENT," +
                                             " name VARCHAR(45) NOT NULL,  lastName VARCHAR(45) NOT NULL," +
                                             " age smallint NOT NULL,  PRIMARY KEY (id));";
@@ -27,11 +28,14 @@ public class UserDaoJDBCImpl implements UserDao {
 
     private void procedureUserTable(String sql_create_table) {
         try {
-            connection.setAutoCommit(false);
-            preparedStatement = connection.prepareStatement(sql_create_table);
-            preparedStatement.execute();
-            connection.commit();
-        } catch (SQLException e) {
+            if (connection != null) {
+                connection.setAutoCommit(false);
+                preparedStatement = connection.prepareStatement(sql_create_table);
+                preparedStatement.execute();
+                connection.commit();
+                connection.setAutoCommit(true);
+            }
+        } catch (SQLException e ) {
             try {
                 connection.rollback();
             } catch (SQLException ex) {
@@ -51,15 +55,19 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void saveUser(String name, String lastName, byte age) {
         try {
-            preparedStatement = connection.prepareStatement(SQL_INSERT_NEW_ROW);
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, lastName);
-            preparedStatement.setByte(3, age);
+            if (connection != null) {
+                connection.setAutoCommit(false);
+                preparedStatement = connection.prepareStatement(SQL_INSERT_NEW_ROW);
+                preparedStatement.setString(1, name);
+                preparedStatement.setString(2, lastName);
+                preparedStatement.setByte(3, age);
 
-            preparedStatement.execute();
-            connection.commit();
+                preparedStatement.executeUpdate();
+                connection.commit();
+                connection.setAutoCommit(true);
 
-            System.out.printf("User с именем – '%s' добавлен в базу данных\n", name);
+                System.out.printf("User с именем – '%s' добавлен в базу данных\n", name);
+            }
         } catch (SQLException e) {
             try {
                 connection.rollback();
@@ -78,21 +86,26 @@ public class UserDaoJDBCImpl implements UserDao {
 
         List<User> users = new ArrayList<>();
         try {
-            preparedStatement = connection.prepareStatement(SQL_SELECT_ROWS);
-            ResultSet resultSet = preparedStatement.executeQuery();
+            if (connection != null) {
+                connection.setAutoCommit(false);
+                preparedStatement = connection.prepareStatement(SQL_SELECT_ROWS);
+                ResultSet resultSet = preparedStatement.executeQuery();
 
-            while (resultSet.next()) {
-                User user = new User(resultSet.getString("name"),
-                                        resultSet.getString("lastName"),
-                                        (byte) resultSet.getInt("age"));
-                user.setId(resultSet.getLong("id"));
-                users.add(user);
+                while (resultSet.next()) {
+                    User user = new User(resultSet.getString("name"),
+                            resultSet.getString("lastName"),
+                            (byte) resultSet.getInt("age"));
+                    user.setId(resultSet.getLong("id"));
+                    users.add(user);
+                }
 
-                System.out.println(user);
+                connection.commit();
+                connection.setAutoCommit(true);
+
+                for (User user : users) {
+                    System.out.println(user);
+                }
             }
-
-            connection.commit();
-
         } catch (SQLException e) {
             try {
                 connection.rollback();
@@ -101,6 +114,7 @@ public class UserDaoJDBCImpl implements UserDao {
             }
             e.printStackTrace();
         }
+        
         return users;
     }
 
