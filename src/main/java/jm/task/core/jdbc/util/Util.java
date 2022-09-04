b61efdadf3dbe86b233cb4141ee6a59827d8ee94
Mike-Sharin <109241989+Mike-Sharin@users.noreply.github.com>
2022-09-04 15:29:27 +0300

    Решение задачи pre-project 1.1.4

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,13 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соеденения с БД
@@ -18,6 +23,30 @@ public class Util {
         try {
             return DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        try {
+            Properties properties = new Properties();
+
+            properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+
+            properties.put(Environment.URL, URL);
+            properties.put(Environment.USER, USERNAME);
+            properties.put(Environment.PASS, PASSWORD);
+
+            properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+
+            properties.put(Environment.SHOW_SQL, true);
+            properties.put(Environment.FORMAT_SQL, true);
+            properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+            //hibernate.hbm2ddl.auto=create-drop
+
+            return new Configuration().addProperties(properties).addAnnotatedClass(User.class).buildSessionFactory();
+        } catch (Exception e){
             e.printStackTrace();
         }
         return null;
